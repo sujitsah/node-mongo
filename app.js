50483b0b231f0882735800b949a69becaddb4ea5
sujit sah <sujitsah60@gmail.com>
2019-06-21 01:17:53 +0530

    creating render

--- app.js
+++ app.js
@@ -6,25 +6,18 @@ const app = express();
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
-// app.use(function(req,res,next){
-//     // console.log('time',Date.now());
-//     req.name='Sujit';
-
-//     next();
-// });
-
-
-//index
 app.get('/',(req,res)=>{
     console.log(req.name);
-    res.send('index');
+    const title = 'welcome2';
+    res.render('index',{
+        title: title
+    });
 });
 
 //about
 
 app.get('/about',(req,res)=>{
-    res.send('About');
+    res.render('About');
 });
 
 
