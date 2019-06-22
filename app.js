a00b6409a8edbf8390ab68027a0123e4c46fc53d
sujit sah <sujitsah60@gmail.com>
2019-06-22 17:58:23 +0530

    bootstrap initaited

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 
 const app = express();
 
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     console.log(req.name);
     const title = 'welcome2';
-    res.render('index',{
+    res.render('index', {
         title: title
     });
 });
 
 //about
 
-app.get('/about',(req,res)=>{
+app.get('/about', (req, res) => {
     res.render('About');
 });
 
@@ -25,6 +25,6 @@ app.get('/about',(req,res)=>{
 
 const port = 5000;
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`app listeneing to port ${port}`)
 });
