65a0c73328b3da60342ecb011a6daaf5cc3879d2
sujit sah <sujitsah60@gmail.com>
2019-06-24 00:33:40 +0530

    saving ideas to local db

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 var exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
 const mongoose = require('mongoose');
 
@@ -13,10 +14,20 @@ mongoose.connect('mongodb://localhost/vidjot-dev',{
 .then(()=>{console.log('mongodb connected')})
 .catch(err => console.log(err));
 
-const app = express();
+//load the mongoose
+require('./models/idea');
+const Idea = mongoose.model('ideas');
 
+
+
+const app = express();
+//handle bar middleware
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 
 app.get('/', (req, res) => {
     console.log(req.name);
@@ -32,8 +43,55 @@ app.get('/about', (req, res) => {
     res.render('About');
 });
 
+//load ideas
+app.get('/ideas/add',(req,res)=>{
+    res.render('ideas/add');
+});
 
+//process form
+app.post('/ideas',(req,res)=>{
+    let errors = [];
 
+    if(!req.body.title){
+        errors.push({text: 'please add title'});
+    }
+    if(!req.body.details){
+        errors.push({text: 'please add details'});
+    }
+    if(errors.length > 0){
+        res.render('ideas/add',{
+            errors:errors,
+            title: req.body.title,
+            details: req.body.details
+        });
+    }
+    else
+    {
+    const newUser={
+            title:req.body.title,
+            details:req.body.details
+    }
+    new Idea(newUser)
+        .save()
+        .then(idea=>{
+            res.redirect('/ideas');
+
+        })
+    
+    }
+});
+//idea index page
+app.get('/ideas',(req,res)=>{
+    idea.find({})  
+    .sort({date:'desc'})
+    .then(ideas=>{
+        res.render('ideas/index',{
+            ideas:ideas
+        });
+        
+    });
+    
+});
 
 const port = 5000;
 
