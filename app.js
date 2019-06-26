a653721f5066660c12d97a7f896370574625b93b
sujit sah <sujitsah60@gmail.com>
2019-06-26 20:53:21 +0530

    login functionality added

--- app.js
+++ app.js
@@ -1,77 +1,168 @@
 const express = require('express');
-var exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const methodOverride = require('method-override')
+const path = require('path');
+const exphbs  = require('express-handlebars');
+const methodOverride = require('method-override');
 const flash = require('connect-flash');
 const session = require('express-session');
-
-
-//load routes
-const ideas = require('./routes/ideas');
-const users = require('./routes/users');
-//mongoose promise global
-mongoose.Promise = global.Promise;
-
-//connect to mongoose
-mongoose.connect('mongodb://localhost/vidjot-dev', {
-    useNewUrlParser: true
-})
-    .then(() => { console.log('mongodb connected') })
-    .catch(err => console.log(err));
-
-
-
-
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const mongoose = require('mongoose');
 
 const app = express();
-//handle bar middleware
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
+// Load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
+
+// Passport Config
+require('./config/passport')(passport);
+
+// Map global promise - get rid of warning
+mongoose.Promise = global.Promise;
+// Connect to mongoose
+mongoose.connect('mongodb://localhost/vidjot-dev', {
+   useNewUrlParser: true})
+  .then(() => console.log('MongoDB Connected...'))
+  .catch(err => console.log(err));
+
+// Handlebars Middleware
+app.engine('handlebars', exphbs({
+  defaultLayout: 'main'
+}));
 app.set('view engine', 'handlebars');
 
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Method override middleware
 app.use(methodOverride('_method'));
-//express session middleware
+
+// Express session midleware
 app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true,
-  }));
+  secret: 'secret',
+  resave: true,
+  saveUninitialized: true
+}));
 
-  app.use(flash());
+app.use(flash());
 
-  //global variables
-  app.use(function(req,res,next){
-      res.locals.success_msg = req.flash('success_msg');
-      res.locals.error_msg = req.flash('error_msg');
-      res.locals.error = req.flash('error');
-      next();
-    });
+// Global variables
+app.use(function(req, res, next){
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
-
+// Index Route
 app.get('/', (req, res) => {
-    console.log(req.name);
-    const title = 'welcome2';
-    res.render('index', {
-        title: title
-    });
+  const title = 'Welcome';
+  res.render('index', {
+    title: title
+  });
 });
 
-//about
-
+// About Route
 app.get('/about', (req, res) => {
-    res.render('About');
+  res.render('about');
 });
 
 
-
-//routes
+// Use routes
 app.use('/ideas', ideas);
 app.use('/users', users);
+
 const port = 5000;
 
-app.listen(port, () => {
-    console.log(`app listeneing to port ${port}`)
+app.listen(port, () =>{
+  console.log(`Server started on port ${port}`);
 });
+
+
+// const express = require('express');
+// var exphbs = require('express-handlebars');
+// const bodyParser = require('body-parser');
+// const mongoose = require('mongoose');
+// const methodOverride = require('method-override')
+// const flash = require('connect-flash');
+// const session = require('express-session');
+// const passport = require('passport');
+
+
+// //load routes
+// const ideas = require('./routes/ideas');
+// const users = require('./routes/users');
+
+// //loading passport
+
+// require('./config/passport')(passport);
+
+// //mongoose promise global
+// mongoose.Promise = global.Promise;
+
+// //connect to mongoose
+// mongoose.connect('mongodb://localhost/vidjot-dev', {
+//     useNewUrlParser: true
+// })
+//     .then(() => { console.log('mongodb connected') })
+//     .catch(err => console.log(err));
+
+
+
+
+
+// const app = express();
+// //handle bar middleware
+// app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// app.set('view engine', 'handlebars');
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.use(methodOverride('_method'));
+// //express session middleware
+// app.use(session({
+//     secret: 'secret',
+//     resave: true,
+//     saveUninitialized: true,
+//   }));
+
+//   app.use(flash());
+
+//   //global variables
+//   app.use(function(req,res,next){
+//       res.locals.success_msg = req.flash('success_msg');
+//       res.locals.error_msg = req.flash('error_msg');
+//       res.locals.error = req.flash('error');
+//       next();
+//     });
+
+
+// app.get('/', (req, res) => {
+//     console.log(req.name);
+//     const title = 'welcome2';
+//     res.render('index', {
+//         title: title
+//     });
+// });
+
+// //about
+
+// app.get('/about', (req, res) => {
+//     res.render('About');
+// });
+
+
+
+// //routes
+// app.use('/ideas', ideas);
+// app.use('/users', users);
+// const port = 5000;
+
+// app.listen(port, () => {
+//     console.log(`app listeneing to port ${port}`)
+// });
--- routes/users.js
+++ routes/users.js
@@ -4,77 +4,169 @@ const bcrypt = require('bcryptjs');
 const passport = require('passport');
 const router = express.Router();
 
-//user login and register route
-
-router.get('/login', (req, res) => {
-    res.render('users/login');
-});
-router.get('/register', (req, res) => {
-    res.render('users/register');
-});
- 
-
-//load the model
-require('../models/user');
+// Load User Model
+require('../models/User');
 const User = mongoose.model('users');
 
-//registration for post
-router.post('/register', (req, res) => {
-    let errors = [];
-    if (req.body.password != req.body.password2) {
-        errors.push({ text: 'password do not match' });
-    }
-    if (req.body.password.length < 3) {
-        errors.push({ text: 'length is short' });
-    }
+// User Login Route
+router.get('/login', (req, res) => {
+  res.render('users/login');
+});
 
-    if (errors.length > 0) {
-        res.render('users/register', {
-            errors: errors,
+// User Register Route
+router.get('/register', (req, res) => {
+  res.render('users/register');
+});
+
+// Login Form POST
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', {
+    successRedirect:'/ideas',
+    failureRedirect: '/users/login',
+    failureFlash: true
+  })(req, res, next);
+});
+
+// Register Form POST
+router.post('/register', (req, res) => {
+  let errors = [];
+
+  if(req.body.password != req.body.password2){
+    errors.push({text:'Passwords do not match'});
+  }
+
+  if(req.body.password.length < 4){
+    errors.push({text:'Password must be at least 4 characters'});
+  }
+
+  if(errors.length > 0){
+    res.render('users/register', {
+      errors: errors,
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+      password2: req.body.password2
+    });
+  } else {
+    User.findOne({email: req.body.email})
+      .then(user => {
+        if(user){
+          req.flash('error_msg', 'Email already regsitered');
+          res.redirect('/users/register');
+        } else {
+          const newUser = new User({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-            password2: req.body.password2
-
-        });
-    }
-    else {
-
-        User.findOne({email: req.body.email})
-        .then(user =>{
-            if(user){
-                req.flash('error_msg','email already exist');
-                res.redirect('/users/register');
-            }
-            else{
-                const newUser = new User({
-                    name: req.body.name,
-                    email: req.body.email,
-                    password: req.body.password
+            password: req.body.password
+          });
+          
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(newUser.password, salt, (err, hash) => {
+              if(err) throw err;
+              newUser.password = hash;
+              newUser.save()
+                .then(user => {
+                  req.flash('success_msg', 'You are now registered and can log in');
+                  res.redirect('/users/login');
                 })
-                bcrypt.genSalt(10,(err,salt)=>{
-                    bcrypt.hash(newUser.password,salt,(err,hash)=>{
-                        if(err) throw err;
-                        newUser.password = hash;
-        
-                        newUser.save()
-                        .then(user=>{
-        
-                          req.flash('success_msg','you are now registered');
-                          res.redirect('/users/login');  
-                        })
-                        .catch(err=>{
-                            console.log(err);
-                            return;
-                        });
-                    });
+                .catch(err => {
+                  console.log(err);
+                  return;
                 });
-
-            }
-        })
-      
-        
-    }
+            });
+          });
+        }
+      });
+  }
 });
 
 module.exports = router;
+
+
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const bcrypt = require('bcryptjs');
+// const passport = require('passport');
+// const router = express.Router();
+
+// //user login and register route
+
+// router.get('/login', (req, res) => {
+//     res.render('users/login');
+// });
+// router.get('/register', (req, res) => {
+//     res.render('users/register');
+// });
+ 
+// router.post('/login',(req,res,next)=>{
+//     passport.authenticate('local',{
+//         successRedirect:'/ideas',
+//         failureRedirect:'/users/login',
+//         failureFlash: true
+//     })(res,req,next);
+// });
+
+// //load the model
+// require('../models/user');
+// const User = mongoose.model('users');
+
+// //registration for post
+// router.post('/register', (req, res) => {
+//     let errors = [];
+//     if (req.body.password != req.body.password2) {
+//         errors.push({ text: 'password do not match' });
+//     }
+//     if (req.body.password.length < 3) {
+//         errors.push({ text: 'length is short' });
+//     }
+
+//     if (errors.length > 0) {
+//         res.render('users/register', {
+//             errors: errors,
+//             name: req.body.name,
+//             email: req.body.email,
+//             password: req.body.password,
+//             password2: req.body.password2
+
+//         });
+//     }
+//     else {
+
+//         User.findOne({email: req.body.email})
+//         .then(user =>{
+//             if(user){
+//                 req.flash('error_msg', 'email already exist');
+//                 res.redirect('/users/register');
+//             }
+//             else{
+//                 const newUser = new User({
+//                     name: req.body.name,
+//                     email: req.body.email,
+//                     password: req.body.password
+//                 })
+//                 bcrypt.genSalt(10,(err,salt)=>{
+//                     bcrypt.hash(newUser.password,salt,(err,hash)=>{
+//                         if(err) throw err;
+//                         newUser.password = hash;
+        
+//                         newUser.save()
+//                         .then(user=>{
+        
+//                           req.flash('success_msg', 'you are now registered');
+//                           res.redirect('/users/login');  
+//                         })
+//                         .catch(err=>{
+//                             console.log(err);
+//                             return;
+//                         });
+//                     });
+//                 });
+
+//             }
+//         })
+      
+        
+//     }
+// });
+
+// module.exports = router;
